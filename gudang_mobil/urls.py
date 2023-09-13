e0c5ccc28162eefd57001a6eeb0f17383458bab5
FBK15 <asadilhaqklem@gmail.com>
2023-09-13 10:24:58 +0700

    update isi urls.py di gudang_mobil

--- gudang_mobil/urls.py
+++ gudang_mobil/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', include('main.urls')),
-    path('developer/', include('main.urls')),
 ]
